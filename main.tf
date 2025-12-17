e02ea2855f260882a4e79660da994cbbd6522903
kelwin souza santos <kelwin29sk8@gmail.com>
2025-12-17 08:38:51 -0300

    criaçao provders_vpc_bucket s3
    criaçao privders, vpc, bucket s3

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.26.0"
+    }
+  }
+  backend "s3" {
+    bucket = "value"
+    key = "value"
+    region = "value"
+  }
+
+}
+
+provider "aws" {
+  region = "sa-east-1"
+}
