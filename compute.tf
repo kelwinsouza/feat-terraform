465ad9395ffa18fc454564bae82f402246152fcc
kelwin souza santos <kelwin29sk8@gmail.com>
2025-12-19 16:18:12 -0300

    fix(terraform): corrigir referências de rede e reforçar .gitignore
    - Removida dependência de terraform_remote_state
    - Ajustadas referências diretas entre VPC, subnet e security group
    - Adicionada associação de route table à subnet
    - Protegidos arquivos sensíveis no .gitignore

--- compute.tf
+++ compute.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "vm" {
   ami                    = "ami-077aec33f15de0896"
   instance_type          = "t3.micro"
   key_name               = aws_key_pair.key.key_name
-  subnet_id              = data.terraform_remote_state.vpc.outputs.subnet_id
-  vpc_security_group_ids = [data.terraform_remote_state.vpc.outputs.security_group]
+  subnet_id              = aws_subnet.subnet.id
+  vpc_security_group_ids = [aws_security_group.security_group.id]
 
   associate_public_ip_address = true
 
--- network.tf
+++ network.tf
@@ -25,9 +25,13 @@ resource "aws_route_table" "route_table" {
     vpc_id = aws_vpc.main.id
   route{
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.geteway.id
+    gateway_id = aws_internet_gateway.gateway.id
   }
   tags = {
     Name = "route_table_001"
   }
+}
+resource "aws_route_table_association" "subnet_association" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route_table.id
 }
